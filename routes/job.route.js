70141e99e78d8f25e75de6914d3183d96e5f6bbc
Aayush Jain <162476331+Aayushajs@users.noreply.github.com>
2024-11-18 02:46:05 +0530

    Update job.route.js

--- routes/job.route.js
+++ routes/job.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route("/post").post(isAuthenticated, postJob);
 router.route("/get").get(isAuthenticated, getAllJobs);
-router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
+router.route("/get/admin/jobs").get(isAuthenticated, getAdminJobs);
 router.route("/get/:id").get(isAuthenticated, getJobById);
 router.route("/delete/:id").delete(isAuthenticated, deleteJob);
 
